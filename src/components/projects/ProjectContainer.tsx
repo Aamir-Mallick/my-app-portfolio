855bf2b86a874a241e9f295853699982050607c2
Aamir Mallick <aamir.mallick@tetrixx.sg>
2023-11-30 14:07:10 +0530

    added project

--- src/components/projects/ProjectContainer.tsx
+++ src/components/projects/ProjectContainer.tsx
@@ -76,9 +76,9 @@ export const ProjectContainer = (props: shiftProps) => {
                 Starweaver
               </Typography>
               <Typography variant="body2" className={classes.workTextContent}>
-                It was learnig web application, where user can learn from class
-                instructor, it was production application, I have worked on
-                different UI section
+                This is E-learnimg web portal, which enble learning with
+                interactivity for the learner. it Provide an unique learning
+                experience and also real time communication with the instructor.
               </Typography>
               <Box className={classes.projectLinksMobile}>
                 <Box
@@ -329,6 +329,139 @@ export const ProjectContainer = (props: shiftProps) => {
                 </Box>
               </Box>
             </Box>
+          </motion.div>
+        </Box>
+        <Box
+          className={classes.projectMainConatiner}
+          style={{
+            position: "relative",
+          }}
+        >
+          <motion.div
+            initial={{ x: 300, opacity: 0 }}
+            animate={{
+              x: projectTwo ? 0 : 300,
+              opacity: projectTwo ? 1 : 0,
+            }}
+            transition={{ delay: 0, duration: 1 }}
+            className={classes.projectContentContainerReverse}
+          >
+            <Typography
+              className={classes.projectName}
+              variant="h4"
+              align="left"
+            >
+              Starweaver
+            </Typography>
+            <Box className={classes.projectContentBoxReverse}>
+              <Typography
+                className={classes.projectNameMobile}
+                variant="h4"
+                align="right"
+              >
+                Starweaver
+              </Typography>
+              <Typography variant="body2" className={classes.workTextContent}>
+                Poolfish is an all-in-one workspace for liquidity providers —
+                calculate, discover, analyze, manage & track positions, and
+                more.
+              </Typography>
+              <Box className={classes.projectLinksMobile}>
+                <Box
+                  style={{ display: "flex", justifyContent: "space-evenly" }}
+                >
+                  <Typography variant="body2">React.js</Typography>
+                  <Typography variant="body2">Next.js</Typography>
+                  <Typography variant="body2">Tailwind</Typography>
+                  <Typography variant="body2">Typescript</Typography>
+                </Box>
+                <Box
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "1rem",
+                  }}
+                >
+                  <Box
+                    style={{
+                      marginRight: "1.5rem",
+                      color: "#ccd6f6",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <Link href="https://poolfish.xyz/calculators/uniswap">
+                      <AddToHomeScreenIcon
+                        style={{
+                          color: "#ccd6f6",
+                        }}
+                      />
+                    </Link>
+                  </Box>
+                  <Box
+                    style={{
+                      marginRight: "1.5rem",
+                      color: "#ccd6f6",
+                      cursor: "pointer",
+                    }}
+                  >
+                    {/* <GitHubIcon /> */}
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Box className={classes.projectLinks}>
+              <Box style={{ display: "flex", justifyContent: "space-evenly" }}>
+                <Typography variant="body2">React.js</Typography>
+                <Typography variant="body2">Next.js</Typography>
+                <Typography variant="body2">Tailwind</Typography>
+                <Typography variant="body2">Typescript</Typography>
+              </Box>
+              <Box
+                style={{
+                  display: "flex",
+                  justifyContent: "start",
+                  marginTop: "1rem",
+                }}
+              >
+                <Box
+                  style={{
+                    marginRight: "1.5rem",
+                    color: "#ccd6f6",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Link href="https://poolfish.xyz/calculators/uniswap">
+                    <AddToHomeScreenIcon
+                      style={{
+                        color: "#ccd6f6",
+                      }}
+                    />
+                  </Link>
+                </Box>
+                <Box
+                  style={{
+                    marginRight: "1.5rem",
+                    color: "#ccd6f6",
+                    cursor: "pointer",
+                  }}
+                >
+                  {/* <GitHubIcon /> */}
+                </Box>
+              </Box>
+            </Box>
+          </motion.div>
+          <motion.div
+            initial={{ y: 300, opacity: 0 }}
+            animate={{ y: projectTwo ? 0 : 300, opacity: projectTwo ? 1 : 0 }}
+            transition={{ delay: 0, duration: 1 }}
+            className={classes.imageContainer}
+          >
+            <img
+              src={"../../../poolfish.png"}
+              width="100%"
+              height="100%"
+              className={classes.imageBox}
+            />
           </motion.div>
         </Box>
       </Container>
